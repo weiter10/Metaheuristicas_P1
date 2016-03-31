82b929bece52edcb2dc5d6be11ad960f49213464
manu <manu@localhost>
2016-03-31 17:37:37 +0200

    Final version

--- src/pratice1/Main.java
+++ src/pratice1/Main.java
@@ -19,99 +19,60 @@ public abstract class Main
 {
     public static void main(String[] args)
     {
+        ArrayList<Thread> listA = new ArrayList();
         long initial, end;
         Random rnd;
-        RandomSearch rs;
-        LocalSearch ls;
-        SimulatedAnnealing sa;
-        TabuSearch ts;
+        int numExecutions = 10;
+        /*Elegir el fichero a procesar:
+        tai25b
+        sko90
+        tai150b
+        */
+        Parse data = new Parse("tai150b");
         
-        //tai25b
-        //sko90
-        //tai150b
-        Parse data = new Parse("sko90");
+        //Ejecutamos el algoritmo Greedy
         initial = System.currentTimeMillis();
         System.out.println("Greedy: " + TestSolution.test(Greedy.start(data),data));
         end = System.currentTimeMillis();
-        System.out.println("Tiempo: " + (end-initial));
+        System.out.println("Tiempo: " + (end-initial) + "ms");
         System.out.println("---------------------------------------");
+        //--Gready
         
-        /*
-        for(int i = 0; i < 10; i++)
+        //Ejecutamos el algoritmo Seleccionado
+        try
         {
-            try
+            initial = System.currentTimeMillis();
+
+            for (int i = 0; i < numExecutions; i++)
             {
-                mal, crear un random a cada objeto
                 rnd = new Random();
                 rnd.setSeed(i);
-                rs = new RandomSearch(data, rnd);
-                ls = new LocalSearch(data, rnd);
-                sa = new SimulatedAnnealing(data, rnd);
-                ts = new TabuSearch(data, rnd);
-                
-                rs.start();
-                ls.start();
-                sa.start();
-                ts.start();
-                
-                rs.join();
-                ls.join();
-                sa.join();
-                ts.join();
-                
-                
-                System.out.println("Iteracion " + i);
-                System.out.println("Busqueda aleatoria: " + TestSolution.test(rs.getSolution(), data));
-                System.out.println("Busqueda local: " + TestSolution.test(ls.getSolution(), data));
-                System.out.println("Enfriamiento simulado: " + TestSolution.test(sa.getSolution() ,data));
-                System.out.println("Busqueda Tabu: " + TestSolution.test(ts.getSolution() ,data));
-                
-                
-                
-                System.out.println("---------------------------------------");
-            } catch (InterruptedException ex)
-            {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                /*Elegir el algoritmo a ejecutar:
+                RandomSearch
+                LocalSearch
+                SimulatedAnnealing
+                TabuSearch
+                */
+                listA.add(new TabuSearch(data, rnd));
+                listA.get(i).start();
             }
-        }
-*/
-        
-        int countJ = 10;
-        int countI = countJ*1;
-        ArrayList<Thread> listA;
-        
-        for(int i = 0; i < countI; i += countJ)
+
+            for (int i = 0; i < numExecutions; i++) listA.get(i).join();
+
+            end = System.currentTimeMillis();
+
+            for (int i = 0; i < numExecutions; i++)
+            {
+                System.out.println(TestSolution.test(((Searcheable)(listA.get(i))).getSolution(), data));
+            }
+
+            System.out.println("Tiempo medio de una ejecucion: " + (end-initial)/numExecutions + "ms");
+
+        } catch (InterruptedException ex)
         {
-            listA = new ArrayList();
-        
-            try
-            {
-                initial = System.currentTimeMillis();
-                for (int j = 0; j < countJ; j++)
-                {
-                    rnd = new Random();
-                    rnd.setSeed(i+j);
-                    listA.add(new TabuSearch(data, rnd));
-                    listA.get(j).start();
-                }
-                
-                for (int j = 0; j < countJ; j++) listA.get(j).join();
-                
-                end = System.currentTimeMillis();
-                
-                for (int j = 0; j < countJ; j++)
-                {
-                    //System.out.println("Iteracion " + (i+j) + ": " + TestSolution.test(((Searcheable)(listA.get(j))).getSolution(), data));
-                    System.out.println(TestSolution.test(((Searcheable)(listA.get(j))).getSolution(), data));
-                }
-                
-                System.out.println("Tiempo: " + (end-initial));
-                
-            } catch (InterruptedException ex)
-            {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
+        //--
         
     }
 }
